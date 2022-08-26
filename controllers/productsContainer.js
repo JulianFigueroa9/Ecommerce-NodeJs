a1884ab5da520e01058bac0f29fa4eac8106e781
Julian Figueroa Allende <julianfigueroa9@hotmail.com>
2022-08-25 23:06:44 -0300

    Primera entrega del proyecto final

--- controllers/productsContainer.js
+++ controllers/productsContainer.js
@@ -1,6 +1,6 @@
 const fs= require('fs');
 
-class Container{
+class productsContainer{
     constructor(route){
     this.route = route;
     }
@@ -9,11 +9,13 @@ class Container{
         try {
             let data = await fs.promises.readFile(this.route, 'utf-8');
             let dataParse = JSON.parse(data)
+            let timestamp = Date.now()
+            let code = `EAN${Math.ceil(Math.random()*100 + 1)}`
             if (dataParse.length > 0){
-                await fs.promises.writeFile(this.route,JSON.stringify([...dataParse, {...object, id:dataParse[dataParse.length - 1].id + 1}], null, 2))
+                await fs.promises.writeFile(this.route,JSON.stringify([...dataParse, {...object, timestamp, code, id:dataParse[dataParse.length - 1].id + 1}], null, 2))
                 return console.log(`El ID del objeto es ${dataParse[dataParse.length-1].id + 1}`)
             } else {
-                await fs.promises.writeFile(this.route,JSON.stringify([ {...object, id:1} ], null, 2))
+                await fs.promises.writeFile(this.route,JSON.stringify([ {...object, timestamp, code, id:1} ], null, 2))
                 return console.log(`El ID del objeto es 1`)
             }
            
@@ -106,6 +108,6 @@ class Container{
     }
 }
 
-module.exports= Container
+module.exports= productsContainer
 
 
--- server.js
+++ server.js
@@ -1,21 +1,17 @@
-/* Importing the container.js file and creating a new instance of the Container class. */
-/* const Container = require("./container")
-const products = new Container('./products.txt') */
+
+/* Importing the productsContainer and cartContainer modules and creating new instances of them. */
+const productsContainer = require("./controllers/productsContainer")
+const cartsContainer = require('./controllers/cartContainer.js')
+const products = new productsContainer('./data/products.txt')
+const cart = new cartsContainer('./data/cart.txt')
+const admin = true;
 
 /* Importing the express module and creating a new express application. */
 const express = require('express')
 const app = express()
 const { Router } = express
-const router = Router()
-
-/* Importing the http and socket.io modules and creating new instances of the Server class. */
-const { Server: HTTPServer } = require('http')
-const { Server: IOServer } = require('socket.io')
-
-
-/* Creating a new instance of the HTTPServer class and the IOServer class. */
-const httpServer = new HTTPServer(app)
-const io = new IOServer(httpServer)
+const productsRouter = Router()
+const cartRouter = Router()
 
 /* Telling the server to use the express.json() method, the express.static() method and the
 express.urlencoded() method. */
@@ -26,157 +22,233 @@ app.use(express.urlencoded({ extended:true }))
 const PORT = process.env.PORT || 8080
 
 /* Creating a server and listening to the port 8080. */
-const server = httpServer.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Escuchando en el puerto: ${server.address().port}`)
 })
 
-/* Telling the server to use the pug engine for the views. */
-/* app.set('views', './views')
-app.set('view engine', 'pug') */
+/* ---------------------------- PRODUCTS --------------------------- */
 
-let mensajesChat = []
-let productos = []
-
-io.on("connection",  socket => {
-	console.log("Se contectó un usuario");
-
-	const mensaje = {
-		mensaje: "ok",
-		mensajesChat
-	};
-
-    const producto = {
-        mensaje: "ok",
-        productos
-    }
-
-	socket.emit("mensaje-servidor", mensaje);
-
-	socket.on("mensaje-nuevo", (msg, cb) => {
-		mensajesChat.push(msg);
-		const mensaje = {
-			mensaje: "mensaje nuevo",
-			mensajesChat
-		};
-
-		const id = new Date().getTime();
-		io.sockets.emit("mensaje-servidor", mensaje);
-		cb(id);
-	});
-    
-    socket.emit("producto-servidor", producto)
-    socket.on("producto-nuevo", (prod, cb) => {
-        productos.push(prod)
-        const producto = {
-            mensaje: "producto nuevo",
-            productos
-        };
-
-        const id = new Date().getTime();
-        io.sockets.emit("producto-nuevo", producto)
-        cb(id)
-    })
-
-});
-
-
-/* --------------------------- DESAFIOS ANTERIORES -------------------------- */
-
-/* Getting all the products from the products.txt file and rendering them in the index.pug file. */
-/* router.get('/', async (req, res) => {
+/* Getting all the products from the products.txt file. */
+productsRouter.get('/', async (req, res) => {
     const allProducts = await products.getAll()
-    if (allProducts){
-        res.render('index', {
+    res.json(
             allProducts
-        })
-    } else{
+    )
+})
+
+/* Getting the products by ID. */
+productsRouter.get('/:id', async (req, res) => {
+    const {id} = req.params
+    const productById = await products.getByID(+id)
+    if (id == productById.id){
+        res.json(
+            productById
+        )
+    } else {
         res.json(
             {
-                error:"No existen productos"
+                error:`no existe el producto con id ${id}.`
             }
         )
     }
-}) */
+})
 
+/* Saving the new product in the products.txt file. */
+productsRouter.post('/', async (req, res) => {
+    if(admin){ 
+        const newProduct = req.body
+        await products.save(newProduct)
+        const allProducts = await products.getAll()
+        res.json(
+                allProducts
+        )
+    } else {
+        res.json(
+            {
+                error:"No tienes permiso para agregar productos."
+            }
+        )
+    }
+    
+}) 
 
-/* router.post('/', async (req, res) => {
-    const newProduct = req.body
-    await products.save(newProduct)
-    const allProducts = await products.getAll()
-    res.render('index', {
-        allProducts
-    })
+/* Updating the product by ID. */
+productsRouter.put('/:id', async (req, res) => {
+    const {id} = req.params
+    if(admin){
+        const productById = await products.getByID(+id)
+        const modifiedProduct = req.body
+        await products.updateById( {id:+id, ...modifiedProduct} )
+        if(id == productById.id){
+            res.json(
+                {
+                    message: 'Producto modificado',
+                    modifiedProduct
+                }
+            )
+        } else {
+            res.json(
+                {
+                    error:`No existe el producto con id ${id}.`
+                }
+            )
+        }
+    } else {
+        res.json(
+            {
+                error:"No tienes permiso para actualizar productos."
+            }
+        )
+    }
+})
+
+/* Deleting the product by ID. */
+productsRouter.delete('/:id', async (req, res) => {
+    const {id} = req.params
+    const productById = await products.getByID(+id)
+    if(admin){
+        if(id == productById.id){
+            const remainingProducts = await products.deleteByID(+id)
+                res.json(
+                    {
+                        ok: true,
+                        message: 'El producto se eliminó correctamente',
+                        id: id,
+                        remainingProducts
+                    }
+                )
+        } else{
+            res.json(
+                {
+                    error:`No existe el producto con id ${id}.`
+                }
+            )
+        }
+    } else {
+        res.json(
+            {
+                error:"No tienes permiso para borrar un producto."
+            }
+        )
+    }
+})
+
+/* This is a catch-all route that will be executed if none of the previous routes are matched. */
+productsRouter.get('*', (req, res) => {
+    res.json(
+        {
+            error:"Ruta no implementada."
+        }
+    )
+})
+
+/* Telling the server to use the productsRouter module. */
+app.use('/api/products', productsRouter)
+
+/* ------------------------------------------------------------------------ */
+
+/* ---------------------------------- CARTS --------------------------------- */
+
+/* Getting the products from the cart by ID. */
+cartRouter.get('/:id/products', async (req, res) => {
+    const { id } = req.params;
+	const cartById = await cart.getByID(parseInt(id));
+    if(id == cartById.id){
+        let productList = cartById.products;
+        res.json(
+            productList
+        )  
+    } else {
+        res.json(
+            {
+                error:`No existe el carro con id ${id}.` 
+            }
+        )
+    }
+})
+
+/* Creating a new cart. */
+cartRouter.post('/', async (req, res) => {
+	const newCart = await cart.save(req.body);
+	res.json(
+        {
+            message: "Carrito creado", 
+            newCart
+        }
+    );
 
 })
 
-app.use('/products', router) */
-
-
-/* Getting all the products from products.txt */
-/* router.get('/', async (req, res) => {
-    const allProducts = await products.getAll()
-    res.json(
-        allProducts
-    )
-}) */
-
-/* Getting the product by ID. */
-/* router.get('/:id', async (req, res) => {
-    const {id} = req.params
-    const productsById = await products.getByID(+id)
-    res.json(
-        productsById
-    )
-}) */
-
-/* Saving the new product in the products.txt file. */
-/* router.post('/', async (req, res) => {
-    const newProduct = req.body
-    await products.save(newProduct)
-        res.json({
-            ok: true,
-            message: 'El producto se agregó correctamente',
-            newProduct
-            
-        })
-}) */
-
-/* Updating the product by ID. */
-/* router.put('/:id', async (req, res) => {
-    const {id} = req.params
-    const modifiedProduct = req.body
-    await products.updateById( {id:+id, ...modifiedProduct} )
+/* Adding a product to the cart. */
+cartRouter.post('/:id/products', async (req, res) => {
+    const { id } = req.params
+    const addProduct = req.body
+    const cartById = await cart.getByID(+id)
+    if(id == cartById.id){
+        const productToCart = await cart.addProductToCart(id, addProduct)
+        res.json(
+            productToCart
+        )
+    } else {
         res.json(
             {
-                message: 'Producto modificado',
-                modifiedProduct
+                error:`No existe el carrito con id ${id}.`
             }
         )
-}) */
+    }
+    }
+)
 
-/* Deleting the product by ID. */
-/* router.delete('/:id', async (req, res) => {
-    const {id} = req.params
-    const remainingProducts = await products.deleteByID(+id)
+/* Deleting the cart by ID. */
+cartRouter.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    const cartById = await cart.getByID(+id)
+    if(id == cartById.id){
+        await cart.deleteByID(+id);
         res.json(
             {
-                ok: true,
-                message: 'El producto se eliminó correctamente',
-                id: id,
-                remainingProducts
+                message:"carrito eliminado correctamente."
             }
         )
-}) */
+    } else {
+        res.json(
+            {
+                error:`No existe carrito con id ${id}.`
+            }
+        )
+    }
+})
 
-/* Telling the server to use the router for the /api/products route. */
-/* app.use('/api/products', router) */
+/* Deleting the product from the cart by ID. */
+cartRouter.delete('/:idCart/products/:idProd', async (req, res) => {
+    const { idCart, idProd } = req.params;
+    const cartById = await cart.getByID(+idCart)
+    const productById = await products.getByID(+idProd)
+    if((idCart == cartById.id) && (idProd == productById.id)){
+        await cart.deleteProductById(+idCart, +idProd);
+        res.json(
+            {
+                message:"producto eliminado correctamente."
+            }
+        )
+    } else {
+        res.json(
+            {
+                error:"El id del carrito o de producto es incorrecto."
+            }
+        )
+    }
+})
 
-/* Getting a random product from the products.txt file. */
-/* app.get('/randomProduct', async (req, res) => {
-    let quantity = await products.getLength()
-    let random = Math.floor(Math.random()*quantity)+1
-    let randomProduct = await products.getByID(random)
+
+/* A catch-all route that will be executed if none of the previous routes are matched. */
+cartRouter.get('*', async (req, res) => {
     res.json(
-        randomProduct
+        {
+            error: "Ruta no implementada"
+        }
     )
-}) */
+})
+
+/* Telling the server to use the cartRouter module. */
+app.use('/api/cart', cartRouter)
